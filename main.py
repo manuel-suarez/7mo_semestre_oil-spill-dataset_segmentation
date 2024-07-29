ae0f94c0f66d81b970fb376015d63996e6019094
Arturo Suárez <manuel.suarez@cimat.mx>
2024-07-29 11:59:14 -0600

    Initial version, based on deepvlabv3p example

---
+++ main.py
@@ -0,0 +1,117 @@
+import keras
+from keras import layers
+from keras import ops
+
+import os
+import numpy as np
+from glob import glob
+import cv2
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
+# For data preprocessing
+from tensorflow import image as tf_image
+from tensorflow import data as tf_data
+from tensorflow import io as tf_io
+
+# Models
+from models.deeplabv3p import DeeplabV3Plus
+from data.instance_lhp import train_dataset, val_dataset, HOME_DIR, IMAGE_SIZE, NUM_CLASSES
+
+print("Train Dataset:", train_dataset)
+print("Val Dataset:", val_dataset)
+
+model = DeeplabV3Plus(image_size=IMAGE_SIZE, num_classes=NUM_CLASSES)
+model.summary()
+
+loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(
+    optimizer=keras.optimizers.Adam(learning_rate=0.001),
+    loss=loss,
+    metrics=["accuracy"],
+)
+
+history = model.fit(train_dataset, validation_data=val_dataset, epochs=25)
+
+plt.plot(history.history["loss"])
+plt.title("Training Loss")
+plt.ylabel("loss")
+plt.xlabel("epoch")
+plt.savefig('results/training_loss.png')
+
+plt.plot(history.history["accuracy"])
+plt.title("Training Accuracy")
+plt.ylabel("accuracy")
+plt.xlabel("epoch")
+plt.savefig('results/training_acc.png')
+
+plt.plot(history.history["val_loss"])
+plt.title("Validation Loss")
+plt.ylabel("val_loss")
+plt.xlabel("epoch")
+plt.savefig('results/validation_loss.png')
+
+plt.plot(history.history["val_accuracy"])
+plt.title("Validation Accuracy")
+plt.ylabel("val_accuracy")
+plt.xlabel("epoch")
+plt.savefig('results/validation_acc.png')
+
+# Loading the Colormap
+colormap = loadmat(
+    os.path.join(HOME_DIR, "data", "instance-level_human_parsing/instance-level_human_parsing/human_colormap.mat")
+)["colormap"]
+colormap = colormap * 100
+colormap = colormap.astype(np.uint8)
+
+
+def infer(model, image_tensor):
+    predictions = model.predict(np.expand_dims((image_tensor), axis=0))
+    predictions = np.squeeze(predictions)
+    predictions = np.argmax(predictions, axis=2)
+    return predictions
+
+
+def decode_segmentation_masks(mask, colormap, n_classes):
+    r = np.zeros_like(mask).astype(np.uint8)
+    g = np.zeros_like(mask).astype(np.uint8)
+    b = np.zeros_like(mask).astype(np.uint8)
+    for l in range(0, n_classes):
+        idx = mask == l
+        r[idx] = colormap[l, 0]
+        g[idx] = colormap[l, 1]
+        b[idx] = colormap[l, 2]
+    rgb = np.stack([r, g, b], axis=2)
+    return rgb
+
+
+def get_overlay(image, colored_mask):
+    image = keras.utils.array_to_img(image)
+    image = np.array(image).astype(np.uint8)
+    overlay = cv2.addWeighted(image, 0.35, colored_mask, 0.65, 0)
+    return overlay
+
+
+def plot_samples_matplotlib(display_list, figname, figsize=(5, 3)):
+    _, axes = plt.subplots(nrows=1, ncols=len(display_list), figsize=figsize)
+    for i in range(len(display_list)):
+        if display_list[i].shape[-1] == 3:
+            axes[i].imshow(keras.utils.array_to_img(display_list[i]))
+        else:
+            axes[i].imshow(display_list[i])
+    plt.savefig(figname)
+
+
+def plot_predictions(images_list, colormap, name, model):
+    for idx, image_file in enumerate(images_list):
+        image_tensor = read_image(image_file)
+        prediction_mask = infer(image_tensor=image_tensor, model=model)
+        prediction_colormap = decode_segmentation_masks(prediction_mask, colormap, 20)
+        overlay = get_overlay(image_tensor, prediction_colormap)
+        plot_samples_matplotlib(
+            [image_tensor, overlay, prediction_colormap], os.path.join("results", f"{name}_{idx}.png"), figsize=(18, 14)
+        )
+
+plot_predictions(train_images[:4], colormap, "training", model=model)
+plot_predictions(val_images[:4], colormap, "validation", model=model)
+print("Done!")
